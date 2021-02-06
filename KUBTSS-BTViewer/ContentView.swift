49bb022898f3726fd06680f85bc356e9e6013a4d
das08 <takeda5955@gmail.com>
2021-02-06 23:41:39 +0900

    initial commit

---
+++ KUBTSS-BTViewer/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  KUBTSS-BTViewer
+//
+//  Created by das08 on 2021/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ KUBTSS-BTViewer/KUBTSS_BTViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KUBTSS_BTViewerApp.swift
+//  KUBTSS-BTViewer
+//
+//  Created by das08 on 2021/02/06.
+//
+
+import SwiftUI
+
+@main
+struct KUBTSS_BTViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
